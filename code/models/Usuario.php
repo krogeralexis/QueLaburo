0d8ea0dd211acc61cdd0dc36b3ccd9ae0f55ba81
krogeralexis <kroger.alexis01@gmail.com>
2025-10-20 14:11:33 -0300

    Update Usuario.php

--- code/models/Usuario.php
+++ code/models/Usuario.php
@@ -1,8 +1,8 @@
 <?php
 // models/Usuario.php
 require_once __DIR__ . '/../core/Model.php';
-echo "admin";
-class Usuario extends Model 
+
+class Usuario extends \Core\Model 
 {
     /**
      * Devuelve todos los usuarios registrados en la base de datos.
