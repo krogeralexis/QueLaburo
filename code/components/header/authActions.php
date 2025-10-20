4f4e2553a86f103feaeca2caf4a65ffc34f3a2e6
krogeralexis <kroger.alexis01@gmail.com>
2025-10-20 14:45:44 -0300

    a

--- code/components/header/authActions.php
+++ code/components/header/authActions.php
@@ -1,4 +1,4 @@
 <div class="auth-actions">
-  <a href="Core\Router::url('login/index')" class="btn">Iniciar sesión</a>
-  <a href="Core\Router::url('login/register')" class="btn btn-primary">Únete</a>
+  <a href="\Core\Router::url('login/index')" class="btn">Iniciar sesión</a>
+  <a href="\Core\Router::url('login/register')" class="btn btn-primary">Únete</a>
 </div>
