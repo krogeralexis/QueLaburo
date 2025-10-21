852135aea8d07f5f24fe9eca11f735d4ca9c212e
krogeralexis <kroger.alexis01@gmail.com>
2025-10-20 22:34:34 -0300

    Update authActions.php

--- code/components/header/authActions.php
+++ code/components/header/authActions.php
@@ -1,4 +1,12 @@
+<?php
+// Detectamos si es un móvil por el user agent (simplificado)
+$isMobile = preg_match('/(android|iphone|ipad|ipod|mobile)/i', $_SERVER['HTTP_USER_AGENT']);
+
+?>
+
 <div class="auth-actions">
-  <a href="#" class="btn" id="btnLogin">Iniciar sesión</a>
-  <a href="#" class="btn btn-primary" id="btnRegister">Únete</a>
+    <?php if (!$isMobile): ?>
+        <a href="#" class="btn" id="btnLogin">Iniciar sesión</a>
+    <?php endif; ?>
+    <a href="#" class="btn btn-primary" id="btnRegister">Únete</a>
 </div>
