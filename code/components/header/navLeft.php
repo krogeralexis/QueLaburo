312d6e8b29cc0a7eb3ca1af3335a92d845911372
krogeralexis <kroger.alexis01@gmail.com>
2025-10-20 23:25:11 -0300

    Update navLeft.php

--- code/components/header/navLeft.php
+++ code/components/header/navLeft.php
@@ -7,10 +7,11 @@ $usuario_esta_logeado = isset($_SESSION['usuario']['id']);
   <ul>
     <!-- Solo se ve en mobile -->
     <li class="mobile-only">
-      <a href="css/storage/hamburgesita.svg" class="hamburger" aria-label="Abrir menú">
+      <a href="css/storage/hamburgesita.svg" class="hamburger" aria-label="Abrir menú" id="menuToggle">
         <img src="css/storage/hamburgesita.svg" alt="Menú" style="height: 6vh; width: 6vh;">
       </a>
     </li>
+
     <li><a href="index.php?controller=proveedor&action=index" class="nav-link">Panel de Administrador</a></li>
     <li>
       <a href="#" class="nav-link">
@@ -20,3 +21,6 @@ $usuario_esta_logeado = isset($_SESSION['usuario']['id']);
     </li>
   </ul>
 </nav>
+
+<!-- JS -->
+<script src="js/header.js"></script>
