a65ba08f0d99ce60657c572b97f526e8f7e4abda
krogeralexis <kroger.alexis01@gmail.com>
2025-10-23 14:57:13 -0300

    Mayor Fixes

--- code/index.php
+++ code/index.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once __DIR__ . '/vendor/autoload.php';
+
 require_once __DIR__ . '/core/Router.php';
 require_once __DIR__ . '/config/database.php';
 
