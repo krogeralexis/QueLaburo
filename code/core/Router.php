792f763a36ca67f2f0e37a2c697dcb555e7718eb
krogeralexis <kroger.alexis01@gmail.com>
2025-10-25 13:11:34 -0300

    Update Router.php

--- code/core/Router.php
+++ code/core/Router.php
@@ -18,7 +18,7 @@ class Router {
             if ($parts[0] === 'admin') {
                 $controller = 'adminauth';
                 $action = $parts[1] ?? 'login';
-                echo "Ruta especial detectada: Admin\n";
+                echo $url;
             } else {
                 // URL normal tipo /usuario/index
                 $controller = $parts[0] ?? 'usuario';
