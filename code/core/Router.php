1d57292319dbf5d0d662838a9f9dc5bebe0837b7
krogeralexis <kroger.alexis01@gmail.com>
2025-10-25 15:44:00 -0300

    Update Router.php

--- code/core/Router.php
+++ code/core/Router.php
@@ -15,14 +15,14 @@ class Router {
             $parts = explode('/', trim($url, '/'));
             echo "1";
             // Rutas especiales: /admin o cualquier subruta de admin
-            if ($parts[0] === 'admin') {
+            if ($parts[1] === 'admin') {
                 $controller = 'adminauth';        // controlador para admin login
-                $action = $parts[1] ?? 'login';   // si no hay acción, va a login
+                $action ='login';   // si no hay acción, va a login
                 echo "2";
             } else {
                 // URL normal tipo /usuario/index
                 $controller = $parts[0] ?? 'usuario';
-                $action = $parts[1] ?? 'index';
+                $action = 'index';
                 echo "3";
             }
         
